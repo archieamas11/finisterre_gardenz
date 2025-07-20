066135022e6a8c53752402517995064e28858d4d
archieamas11 <archiealbarico69@gmail.com>
2025-07-21 01:56:02 +0800

    first commit

--- include/config.php
+++ include/config.php
@@ -62,30 +62,30 @@ define('WEBROOT', $protocol . '://' . $host . $base_app_path);
 // define('WEBROOT', $web_root);
 
 // PHPMailer Configuration
-define('EMAIL_HOST', 'smtp.gmail.com');
-define('EMAIL_USERNAME', 'cemeterease.memorial@gmail.com');
-define('EMAIL_PASSWORD', 'ryhw bryw bmjm tscl');
-define('EMAIL_PORT', 587);
-define('EMAIL_FROM', 'cemeterease.memorial@gmail.com');
+// define('EMAIL_HOST', 'smtp.gmail.com');
+// define('EMAIL_USERNAME', 'cemeterease.memorial@gmail.com');
+// define('EMAIL_PASSWORD', 'ryhw bryw bmjm tscl');
+// define('EMAIL_PORT', 587);
+// define('EMAIL_FROM', 'cemeterease.memorial@gmail.com');
 
-// Google API Configuration
-define('GOOGLE_CLIENT_ID', '476653195318-u49fh3rogjraviievtraaa09t7sqv2v0.apps.googleusercontent.com');
-define('GOOGLE_CLIENT_SECRET', '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');
-define('GOOGLE_REDIRECT_URI', WEBROOT . 'login/index.php');
+// // Google API Configuration
+// define('GOOGLE_CLIENT_ID', '476653195318-u49fh3rogjraviievtraaa09t7sqv2v0.apps.googleusercontent.com');
+// define('GOOGLE_CLIENT_SECRET', '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');
+// define('GOOGLE_REDIRECT_URI', WEBROOT . 'login/index.php');
 
-// Cloudinary Configuration
-define('CLOUDINARY_CLOUD_NAME', 'djrkvgfvo');
-define('CLOUDINARY_API_KEY', '695834455173873');
-define('CLOUDINARY_API_SECRET', '98Des_R7jcctIfJy6yA81HPg3po');
+// // Cloudinary Configuration
+// define('CLOUDINARY_CLOUD_NAME', 'djrkvgfvo');
+// define('CLOUDINARY_API_KEY', '695834455173873');
+// define('CLOUDINARY_API_SECRET', '98Des_R7jcctIfJy6yA81HPg3po');
 
-// TextBee API Configuration
-define('TEXTBEE_API_KEY', 'aef5a129-0587-45ef-8909-5946c1dea8b6');
-define('TEXTBEE_API_URL', 'https://api.textbee.dev/api/v1/gateway/devices/6828be8fdb6bef3de8d2fd0b/send-sms');
-define('TEXTBEE_SENDER_ID', SYSTEM_NAME);
+// // TextBee API Configuration
+// define('TEXTBEE_API_KEY', 'aef5a129-0587-45ef-8909-5946c1dea8b6');
+// define('TEXTBEE_API_URL', 'https://api.textbee.dev/api/v1/gateway/devices/6828be8fdb6bef3de8d2fd0b/send-sms');
+// define('TEXTBEE_SENDER_ID', SYSTEM_NAME);
 
-// Pusher Configuration
-define('PUSHER_APP_ID', '2001762');
-define('PUSHER_APP_KEY', '2b048793924fa7a65cda');
-define('PUSHER_APP_SECRET', 'bfc53ba1baef4bb5cef7');
-define('PUSHER_APP_CLUSTER', 'ap3');
+// // Pusher Configuration
+// define('PUSHER_APP_ID', '2001762');
+// define('PUSHER_APP_KEY', '2b048793924fa7a65cda');
+// define('PUSHER_APP_SECRET', 'bfc53ba1baef4bb5cef7');
+// define('PUSHER_APP_CLUSTER', 'ap3');
 ?>
